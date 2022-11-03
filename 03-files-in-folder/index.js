5ff29211eabce64c24bc747062dfb29278a9d16a
Bunchenko <andrew.by.one@gmail.com>
2022-11-03 23:43:19 +0300

    feat: finish task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const { readdir, stat } = require('fs/promises');
+const path = require('path');
+
+async function getFilesInfo() {
+	const folderPath = path.join(__dirname, 'secret-folder');
+	const files = await readdir(folderPath, { withFileTypes: true });
+	const pathsToCheck = files.map((element) => path.join(folderPath, element.name));
+
+	for (let i = 0; i < files.length; i++) {
+		if (!files[i].isDirectory()) {
+			const extension = path.extname(files[i].name);
+			const fileStats = await stat(pathsToCheck[i], (err, stats) => {
+				if (err) console.log(err);
+			});
+
+			console.log(
+				`${path.basename(pathsToCheck[i], extension)} - ${extension} - ${fileStats.size}`
+			);
+		}
+	}
+}
+
+getFilesInfo();
