d832b004e43652424cde3c0729b9193c8495ab6f
Bunchenko <andrew.by.one@gmail.com>
2022-11-05 15:42:29 +0300

    feat: finish task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs');
+const { readdir, mkdir } = require('fs/promises');
+const path = require('path');
 
+const currentPath = path.join(__dirname, 'files');
+
+async function copyFiles() {
+	const files = await readdir(currentPath, (err) => {
+		if (err) throw err;
+	});
+
+	for (const file of files) {
+		const filePath = path.join(currentPath, file);
+
+		fs.copyFile(filePath, path.join(__dirname, 'files-copy', file), (err) => {
+			if (err) throw err;
+		});
+	}
+}
+
+mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+
+copyFiles();
